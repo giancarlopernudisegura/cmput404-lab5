69dd9be9aee402b4cb32d5201eea598afad30dfb
giancarlopernudisegura <pernudig@gmail.com>
2022-02-07 17:22:11 -0700

    create theme

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ SITEURL = 'https://giancarlopernudisegura.github.io/cmput404-lab5/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
